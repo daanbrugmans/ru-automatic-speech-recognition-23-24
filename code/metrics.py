b9d8f839b58e095eccaaeef61fdcea2ded7c70d8
Daan Brugmans <daanbrugmans@gmail.com>
2024-06-06 23:56:20 +0200

    Finished the paper

--- code/metrics.py
+++ code/metrics.py
@@ -38,38 +38,6 @@ def attack_success_rate(asv_model: SpeakerIdentificationModel, adversarial_test_
                 
     return attack_success_rate, misclassifications
 
-def evasion_attack_success_rate(asv_model, perturbed_utterances: list, speaker_ids: list, device, target_label: int) -> float:
-    """Calculates and returns the Attack Success Rate of an Evasion Attack for an ASV model.
-    
-    Taken from the Security & Privacy of Machine Learning course (dr. Picek) and refactored."""
-    
-    correct = 0
-    total = 0
-    misclassifications = 0
-
-    with torch.no_grad():
-        asv_model.model.eval()
-
-        for utterance, speaker_id in tqdm(zip(perturbed_utterances, speaker_ids)):
-            # Use perturbed test image to get predictions of clean model
-            speaker_id = speaker_id.item()
-            utterance = utterance.to(device)
-            utterance = torch.squeeze(utterance)
-            utterance = torch.unsqueeze(utterance, 0)
-            
-            prediction = asv_model.get_speakers_using_waveforms(utterance)[0]
-
-            # Check if the predicted speaker ID is equal to the target speaker ID
-            if speaker_id != target_label:
-                total += 1
-                
-                if prediction == target_label:
-                    correct += 1
-
-    attack_success_rate = correct / total    
-                
-    return attack_success_rate, misclassifications
-
 def clean_accuracy_drop(clean_model: SpeakerIdentificationModel, adversarial_model: SpeakerIdentificationModel, clean_test_set: Dataset) -> float:
     """Calculates and returns the Clean Accuracy Drop between a clean and adversarial ASV model."""
     
